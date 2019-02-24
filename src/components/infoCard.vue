073540ef1c4baf62180589c48b21726f33e8662a
12q1 <thomasham89@gmail.com>
2019-02-24 22:31:32 +0100

    moved infocard mockup to its own component

---
+++ src/components/infoCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <q-card>
+    <q-item>
+    <q-item-side avatar="/statics/icons/icon-384x384.png"/>
+    <q-item-main>
+        <q-card-title>Kira<p slot="subtitle">It's How You Know What's In Your Contacts</p></q-card-title>
+    </q-item-main>
+    </q-item>
+    <q-card-separator/>
+    <q-card-main>
+    <p>Kira is a powerful machine learning software that identifies, extacts, and analyzes text in your contracts and other documents</p>
+    </q-card-main>
+    </q-card>
+</template>
+<script>
+export default {
+  name: 'infoCard'
+}
+</script>
+<style>
+/* This is where your CSS goes */
+</style>
--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,31 +1,16 @@
 <template>
-  <q-layout>
-    <q-card>
-    <q-item>
-    <q-item-side avatar="/statics/icons/icon-384x384.png"/>
-    <q-item-main>
-        <q-card-title>Kira<p slot="subtitle">It's How You Know What's In Your Contacts</p></q-card-title>
-    </q-item-main>
-    </q-item>
-    <q-card-separator/>
-    <q-card-main>
-    <p>Kira is a powerful machine learning software that identifies, extacts, and analyzes text in your contracts and other documents</p>
-    </q-card-main>
-    </q-card>
-  </q-layout>
+  <div>
+  <p>Featured</p>
+  <InfoCard/>
+  </div>
 </template>
 
 <script>
-import { openURL } from 'quasar'
-
+import InfoCard from '../components/infoCard'
 export default {
   name: 'MyLayout',
-  data () {
-    return {
-    }
-  },
-  methods: {
-    openURL
+  components: {
+    InfoCard
   }
 }
 </script>
